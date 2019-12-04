8e711bb8103047a80ee9b3eb81320338f0c26ea7
girguy <girineza.guy@gmail.com>
2019-12-04 10:05:19 +0100

    .sh file added

---
+++ Tf-idf/runSvm.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#SBATCH -N 1      # nodes requested
+#SBATCH -n 1      # tasks requested
+#SBATCH -c 4      # cores requested
+#SBATCH --mem=10  # memory in Mb
+#SBATCH -o outfileSvmTfidf  # send stdout to outfile
+#SBATCH -e errfileSvmTfidf  # send stderr to errfile
+#SBATCH -t 0:01:00  # time requested in hour:minute:second
+
+module load Python/2.7.14-foss-2018a
+module load pandas/0.19.1-intel-2016b-Python-2.7.12
+python svm_tfidf.py ./project/data/train.csv
