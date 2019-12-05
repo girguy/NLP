bbce90b1eb220c8961c109ce3469739cf372dc3b
girguy <girineza.guy@gmail.com>
2019-12-05 22:59:42 +0100

    Code correction

--- Part of speech and Tf-idf/svm_pos_tfidf.py
+++ Part of speech and Tf-idf/svm_pos_tfidf.py
@@ -3,17 +3,15 @@
 """
 Created on Sat Nov 16 15:47:44 2019
 
-@author: cj
+@author: Guy Girineza
 """
 
 import sys
 import os
 import re
-import string
 import pandas as pd
 pd.options.mode.chained_assignment = None
 import nltk
-import scipy as scipy
 
 
 """
@@ -22,6 +20,7 @@ Remove HTML entities from the comments
 def clean_html(comment):
     cleaner = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
     cleaned = re.sub(cleaner, '', comment)
+    
     return cleaned
 
 """
@@ -32,6 +31,7 @@ def clean_punc(comment): #function to clean the word of any punctuation or speci
     cleaned = re.sub(r'[.|,|)|(|\|/]',r' ',cleaned)
     cleaned = cleaned.strip()
     cleaned = cleaned.replace("\n"," ")
+    
     return cleaned
 
 """
@@ -44,6 +44,7 @@ def clean_non_alpha(comment):
         cleaned = cleaned + clean_word
         cleaned = cleaned + " "
     cleaned = cleaned.strip()
+    
     return cleaned
 
 """
@@ -63,6 +64,7 @@ Remove stop word
 """
 def remove_stop_words(comment):
     global list_stop_words
+    
     return list_stop_words.sub(" ", comment)
 
 
@@ -76,6 +78,7 @@ def stemming(comment):
         stemSentence += stem
         stemSentence += " "
     stemSentence = stemSentence.strip()
+    
     return stemSentence
 
 
@@ -102,7 +105,7 @@ def tokenization_pos_tag(corpus):
         tokenized = word_tokenize(sentence)
         X.append(nltk.pos_tag(tokenized))
     
-    return Xs
+    return X
 
 """
 Bag of word of tagged corpus
@@ -118,26 +121,20 @@ def dataset_creation(data):
     
     return dataset
 
-
-
-
 """
 This function return a document-word matrix computed for TFIDF
 """
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 def function_tfidf(train, test):
-    #In order to use TfidfVectorizer, pandas dataframe is converted in a list
-    text_train = train["comment_text"].values.tolist()
-    text_test = test["comment_text"].values.tolist()
 
     tfidf_vectorizer = TfidfVectorizer(strip_accents='unicode', analyzer='word',
 				       ngram_range=(1,3), norm='l2', max_features=5000)
 
-    tfidf_vectorizer.fit(text_train)
-    tfidf_vectorizer.fit(text_test)
+    tfidf_vectorizer.fit(train)
+    tfidf_vectorizer.fit(test)
 
-    return tfidf_vectorizer.transform(text_train), tfidf_vectorizer.transform(text_test)
+    return tfidf_vectorizer.transform(train), tfidf_vectorizer.transform(test)
 
 from sklearn.preprocessing import MultiLabelBinarizer
 
@@ -149,20 +146,18 @@ Transform the labels format in order to use it in the machine learning api.
 def function_labels(data):
     #In order to use TfidfVectorizer, pandas dataframe is converted in a list
     data = data.values.tolist()
+    
     return y_mlb.fit_transform(data)
-
-
 
 
 
 if __name__ == "__main__":
 
     input_train_path = sys.argv[1]
+    print(input_train_path)
     if not os.path.exists(input_train_path):
         print("This 'train' file do not exit")
         sys.exit(1)
-
-    print("Ficher charge")
     
     data = pd.read_csv(input_train_path)
     
@@ -185,20 +180,18 @@ if __name__ == "__main__":
     train_data = corpus_cleaning(train_data)
     test_data = corpus_cleaning(test_data)
 
-    train_data_tagged = tokenization_pos_tag(train_data["comment_text"])
-    test_data_pos = tokenization_pos_tag(test_data["comment_text"])
+    train_data = tokenization_pos_tag(train_data["comment_text"])
+    test_data = tokenization_pos_tag(test_data["comment_text"])
 
-    X_train = dataset_creation(train_data_tagged)
-    X_test = dataset_creation(test_data_pos)
-    
-    from sklearn.feature_extraction.text import CountVectorizer
+    X_train = dataset_creation(train_data)
+    X_test = dataset_creation(test_data)
 
-    X_train_tfidf, X_test_tfidf = function_tfidf(train_data, test_data)
+    X_train_tfidf, X_test_tfidf = function_tfidf(X_train, X_test)
 
     from skmultilearn.problem_transform import BinaryRelevance
-    from sklearn.naive_bayes import GaussianNB
+    from sklearn.svm import LinearSVC
     
-    classifier = BinaryRelevance(GaussianNB())
+    classifier = BinaryRelevance(LinearSVC())
     classifier.fit(X_train_tfidf, train_labels)
     
     # predict
